4ac66ba13392743d952d4d43f3a05f9f6146939e
athithi <kkconstruction1000_bec27@mepcoeng.ac.in>
2026-03-03 11:14:34 +0530

    Add stack ADT for managing agricultural water footprints
    Implement a stack-based ADT to manage water footprints for agricultural products, including push, display, and pop functionalities.

---
+++ stack_ADT.cpp
@@ -0,0 +1,110 @@
+//PROBLEM STATEMENT:Calculate Water Footprints for different Agricultural Products
+#include<iostream>
+#define max 10
+using namespace std;
+
+class Footprint {
+private:
+    struct node {
+        string name;
+        string brand;
+        string place;
+        int waterconsumption;
+        string watersource;
+    } x[max];
+    int top;
+public:
+    Footprint() {
+        top=-1;
+    }
+    void push(string,string,string,int,string);
+    void display();
+    void POP();
+};
+
+void Footprint::push(string nn, string b, string pl, int c, string s) {
+    if (top == max - 1) {
+        cout << "\nSorry List is full; can't push";
+        return;
+    }
+    ++top;
+    x[top].name = nn;
+    x[top].brand = b;
+    x[top].place = pl;
+    x[top].waterconsumption = c;
+    x[top].watersource = s;
+}
+
+void Footprint::display(){
+    if(top==-1){
+        cout<<"\nSorry List is empty; can't print";
+        return;
+    }
+    for(int i=top;i>=0;i--){
+      cout<<x[i].name            <<"\t";
+      cout<<x[i].brand           <<"\t";
+      cout<<x[i].place           <<"\t";
+      cout<<x[i].waterconsumption<<"\t\t\t";
+      cout<<x[i].watersource     <<"\t\n";
+    }
+}
+
+void Footprint::POP(){
+    if(top==-1){
+        cout<<"\nSorry Stack Underflow; can't pop";
+        return;
+    }
+    cout << "Popped: " << x[top].name << ", " << x[top].brand << ", " 
+         << x[top].place << ", " << x[top].waterconsumption << ", " 
+         << x[top].watersource << endl;
+    --top;  
+}
+
+int main() {
+    Footprint a;
+    int choice;
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Push\n";
+        cout << "2. Display\n";
+        cout << "3. POP\n";
+        cout << "4. Exit\n";
+        cout << "\nEnter your choice: \n";
+        cin >> choice;
+        cout << "\n";
+        switch (choice) {
+        case 1: {
+            string name, brand, place, watersource;
+            int waterconsumption;
+            cout << "Enter product name: ";
+            cin >> name;
+            cout << "Enter brand: ";
+            cin >> brand;
+            cout << "Enter place: ";
+            cin >> place;
+            cout << "Enter total waterconsumption: ";
+            cin >> waterconsumption;
+            cout << "Enter water source: ";
+            cin >> watersource;
+            a.push(name, brand, place, waterconsumption, watersource);
+            break;
+        }
+        case 2: {
+            a.display();
+            break;
+        }
+
+        case 3: {
+            a.POP();
+            break;
+        }
+        case 4: {
+            cout << "Exit...\n";
+            break;
+        }
+        default:
+            cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 4);
+    return 0;
+}
